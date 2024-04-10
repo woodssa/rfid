ab2941d47f218442d9c0dddbc4a4fcee26f9fc60
Sarah <sarahwoods2226@gmail.com>
2024-04-10 15:14:59 -0700

    updates

--- adafruit-pn532.py
+++ adafruit-pn532.py
@@ -9,7 +9,13 @@ from digitalio import DigitalInOut
 from adafruit_pn532.spi import PN532_SPI
 
 spi = busio.SPI(board.SCK, board.MOSI, board.MISO)
-cs_pin = DigitalInOut(board.D5)
+cs_pin = DigitalInOut(board.D6)
 pn532 = PN532_SPI(spi, cs_pin, debug=False)
 
-uid = pn532.read_passive_target(timeout=0.5)
+pn532.SAM_configuration()
+while True:
+    uid = pn532.read_passive_target(timeout=0.5)
+    print('.', end="", flush=True)
+    if uid is None:
+        continue
+    print('Found card with UID:', [hex(i) for i in uid])
